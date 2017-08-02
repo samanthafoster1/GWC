f47b9fcf829b839ab6fac1400b5025c31f0fc5c2
Bella <mendoza.isabella991@student.wuhsd.org>
2017-08-02 14:59:30 -0700

    fix matches

--- amp/views.py
+++ amp/views.py
@@ -3,28 +3,34 @@ from amp.models import Playlist
 import spotipy
 import spotipy.util as util
 from social_django.models import UserSocialAuth
+from django.contrib.auth.decorators import login_required
 
 
 def index (request) :
     user = request.user
+    print(user)
+    playlists = []
+    social_login = None
     token = None
-    try:
-        spotify_login = user.social_auth.get(provider='spotify')
-    except UserSocialAuth.DoesNotExist:
-        spotify_login = None
-    if spotify_login:
-        token = spotify_login.extra_data['access_token']
-    if token:
-        sp = spotipy.Spotify(auth=token)
-        playlists = sp.user_playlists(user)
-        for playlist in playlists['items']:
-            p = Playlist()
-            p.playlist_id = playlist['id']
-            p.username = user
-            p.name = playlist['name']
-            p.save()
-    playlists = Playlist.objects.all()
-    return render (request, 'amp/home.html', { 'playlists': playlists})
+    if user.is_authenticated:
+        try:
+            if user.social_auth:
+                spotify_login = user.social_auth.get(provider='spotify')
+        except UserSocialAuth.DoesNotExist:
+            spotify_login = None
+        if spotify_login:
+            token = spotify_login.extra_data['access_token']
+        if token:
+            sp = spotipy.Spotify(auth=token)
+            playlists = sp.user_playlists(user)
+            for playlist in playlists['items']:
+                p = Playlist()
+                p.playlist_id = playlist['id']
+                p.username = user
+                p.name = playlist['name']
+                p.save()
+            playlists = Playlist.objects.all()
+    return render (request, 'amp/home.html', { 'playlists': playlists, 'social_login': social_login})
 
 # Create your views here.
 def playlist(request, pk) :
